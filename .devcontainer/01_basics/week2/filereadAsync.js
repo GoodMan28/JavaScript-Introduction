d08aec177370960e648e2a5ec547ac5bfe69ea4f
GoodMan28 <abhineetanand91@gmail.com>
2025-03-10 07:59:20 +0000

    new

--- .devcontainer/01_basics/week2/filereadAsync.js
+++ .devcontainer/01_basics/week2/filereadAsync.js
@@ -4,11 +4,13 @@
 let fs = require("fs")
 
 // // Method 1
-// fs.readFile(".devcontainer/01_basics/week2/a.txt", "utf-8", function (err, contents) {
-//     console.log(contents);
-// });
+fs.readFile(".devcontainer/01_basics/week2/b.txt", "utf-8", function (err, contents) {
+    console.log(contents);
+});
 
 // Method 2
+
+// defining the callback
 function callback(err, contents) {
     if(err) {
         console.log("Error is: " + err);
@@ -17,5 +19,39 @@ function callback(err, contents) {
         console.log(contents);
     }
 }
-
+// this function takes a function name as an argument
+// the readfile function calls the function when the file reading is complete
 fs.readFile(".devcontainer/01_basics/week2/a.txt", "utf-8", callback);
+
+
+// Method 3: There is a method where we call run a callback after certain time
+
+function run() {
+    console.log("I will run");
+}
+
+setTimeout(run, 5000); // setTimeout is another asynchronous function that executes a certain code after some time
+// The setTimeout function will take function as an arg (callback)
+// and it will call the function after 5000 ms = 5 s
+
+
+
+// -----------------------------------------------------------------------------------------------
+
+console.log("It is a normal log statement and will execute the first");
+ // This can be run even before all the statements above
+// since all the tasks are I/O bound tasks and take a few time to run (i.e. read a file)
+sum = 0;
+for(i = 0; i < 1000000000; i++) {
+    sum++;
+}
+console.log(sum); // these all are CPU intensive tasks and hence the thread is busy for logging the pending tasks
+
+
+// Yes, exactly! Even if the file is completely read while the for loop is running, the file content will not be logged immediately.
+
+// This happens because:
+
+// The event loop is blocked due to the long-running for loop.
+// Asynchronous tasks (like fs.readFile) cannot execute their callbacks until the event loop is free.
+// Once the for loop finishes, only then the event loop processes pending tasks, including the file read callback.
