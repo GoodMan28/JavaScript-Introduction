bd494b4a36e66a9f7d7a6b36d9c0a93fcbda592f
GoodMan28 <abhineetanand91@gmail.com>
2025-03-18 15:46:46 +0000

    jj

--- .devcontainer/01_basics/week2/cleanFilePromisifed.js
+++ .devcontainer/01_basics/week2/cleanFilePromisifed.js
@@ -1,12 +1,28 @@
 let fs = require('fs');
-function writeCall(err) {
-    console.log("File written/cleaned successfully");
-    console.log("New content: ");
-    console.log();
-    
+// function writeCall(err) {
+//     if(err) {
+//         return;
+//     }
+//     console.log("File written/cleaned successfully");    
+// }
+// function callBack(err, content) {
+//     if (err) {
+//         console.error("Error reading file:", err);
+//         return;
+//     }
+//     console.log("File read properly");
+//     fs.writeFile('/workspaces/JavaScript-Introduction/.devcontainer/01_basics/week2/a.txt', content.trim(), 'utf-8', writeCall);
+// }
+// fs.readFile("/workspaces/JavaScript-Introduction/.devcontainer/01_basics/week2/a.txt", "utf-8", callBack);
+
+function readFile(fileName) { 
+    return new Promise(); // returns a promise 
 }
-function callBack(err, content) {
-    console.log("File read properly");
-    fs.writeFile('.devcontainer/01_basics/week2/a.txt', content.trim(), 'utf-8', writeCall);
+
+const p = readFile("/workspaces/JavaScript-Introduction/.devcontainer/01_basics/week2/a.txt");
+
+function callBack(content) {
+    console.log(content);
 }
-let content = fs.readFile(".devcontainer/01_basics/week2/a.txt", "utf-8", callBack);
+
+p.then(callBack);
